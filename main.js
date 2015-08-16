499f0f9024a5ff462ab25065fca0e4d6d5c771f7
GyozaGuy <gyozaguy@gmail.com>
2015-08-15 19:01:54 -0600

    Added "media" permission for microphone use

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function updateWebviews() {
-	var webview = document.querySelector("webview");
+	var webview = document.querySelector('webview');
 	webview.style.height = document.documentElement.clientHeight + 'px';
 	webview.style.width = document.documentElement.clientWidth + 'px';
 }
 
-// document.querySelector("webview").addEventListener('permissionrequest', function(e) {
-// 	if (e.permission === 'media') {
-// 		e.request.allow();
-// 	}
-// }
+document.querySelector('webview').addEventListener('permissionrequest', function(e) {
+	if (e.permission === 'media') {
+		e.request.allow();
+	}
+});
 
 onload = updateWebviews;
 window.onresize = updateWebviews;
