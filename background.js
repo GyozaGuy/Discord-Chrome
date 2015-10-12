8ad3f8f0700e92a8f1112537527c696e771c61db
Steven Stromberg <steven.stromberg@intel.com>
2015-10-12 09:24:12 -0600

    Reformatting, changed partition ID

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('index.htm', {
-		bounds: {
-			height: 700,
-			width: 1200
-		},
-		id: 'discord'
-	});
+	chrome.app.window.create(
+		'index.htm',
+		{id: 'discord', height: 700, width: 1200}
+	);
 });
