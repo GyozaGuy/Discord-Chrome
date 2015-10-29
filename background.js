1aedb21b0cf2b67d4108fa9b1da4ca5188ec4e22
Steven Stromberg <steven.stromberg@intel.com>
2015-10-29 10:19:00 -0600

    Window stays centered

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
+var height = 700;
+var width = 1200;
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create(
-		'index.htm',
-		{id: 'discord', height: 700, width: 1200}
-	);
+	chrome.app.window.create('index.htm',	{
+	  id: 'discord',
+	  outerBounds: {
+	    height: height,
+	    width: width
+	  }
+	},
+	function(win) {
+    win.outerBounds.setPosition(
+      Math.round(screen.availWidth / 2 - width / 2),
+      Math.round(screen.availHeight / 2 - height / 2)
+    );
+	});
 });
